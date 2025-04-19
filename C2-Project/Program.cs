dae98c13e175a2567bbe402ba47972d92fefa71b
AnnaCharbadze <126344444+AnnaCharbadze@users.noreply.github.com>
2025-04-18 17:52:57 -0700

    Update Program.cs

--- C2-Project/Program.cs
+++ C2-Project/Program.cs
@@ -1,51 +1,37 @@
 using System;
 using School.Models;
 using School.Controllers;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// Swagger API help pages
+// Swagger API 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 // Database
 builder.Services.AddScoped<SchoolDbContext>();
-
 // API Controllers
 builder.Services.AddScoped<TeacherAPIController>();
-
-// Add the TeacherController
-builder.Services.AddScoped<TeacherController>();
-
+// Add the TeacherPageController
+builder.Services.AddScoped<TeacherPageController>();
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-   
+  
     app.UseHsts();
 }
 else
 {
     app.UseDeveloperExceptionPage();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
